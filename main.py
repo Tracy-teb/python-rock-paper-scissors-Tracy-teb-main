3990ca018c815825a9da8cc2cb72eca79b223c24
Tracy-teb <tracymiriti17@gmail.com>
2024-05-03 19:36:31 +0300

    Function to play game

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #import random choice from python library
 from random import choice
 
+#define the choices
+choices = ["rock", "paper", "scissors"]
+
 #defining a function and rules of the game
 def game_winner (computer_choice, human_choice):
     if computer_choice == human_choice:
@@ -8,19 +11,49 @@ def game_winner (computer_choice, human_choice):
     elif ((computer_choice == 'rock' and human_choice == 'scissors') or
           (computer_choice == 'paper' and human_choice == 'rock') or
           (computer_choice == 'scissors' and human_choice == 'paper')):
-        return f'computer chose {computer_choice}. computer wins!'
+        return computer_choice
     else:
-        return f'computer chose{computer_choice}. human wins!'
+        return human_choice
     
-#Get ccomputer choice 
-computer_choice = choice(['rock', 'paper', 'scissors'])
+#Function to play the game 
+def play_game(max_rounds):
+    human_score = 0
+    computer_score = 0
+    round_count = 0
 
-#Get human choice 
-human_choice = input("enter 'rock', 'paper', or 'scissors':")
+    while round_count < max_rounds:
+        round_count += 1
+        computer_choice = choice(choices)
+        human_choice = input(f"round{round_count}: 'rock','paper', or 'scissors': ")
 
-#Call the result
-result = game_winner(computer_choice, human_choice)
+        if human_choice not in choices:
+            print("Invalid choice. Try Again.")
+            continue
 
-print(result)
+        result = game_winner(computer_choice, human_choice)
+
+        if result == "computer":
+            computer_score += 1
+            print(f"You chose{human_choice}, and the computer chose{computer_choice}. The computer wins this round!")
+        elif result == "human":
+            human_score += 1
+            print(f"You chose{human_choice}, and the computer chose{computer_choice}. You win this round!")
+        else:
+            print(f"You both chose {human_choice}. It is a tie!")
+
+        print(f"score: You {human_score} - computer {computer_score}")
+
+        if human_score == max_rounds // 2 + 1 or computer_score == max_rounds // 2 + 1:
+            break
+
+        if human_score > computer_score:
+            print("Congratulations ! You win the game!")
+        elif human_score < computer_score:
+            print("Sorry, the computer wins the game.")
+        else:
+            print("It is a tie game!")
+
+play_game(5)
+
 
 
